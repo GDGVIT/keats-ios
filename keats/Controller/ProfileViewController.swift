71b1a2763546c69f28e5b24cc31999659cd0f5ca
Swamita <swamita2001@gmail.com>
2021-04-06 15:45:20 +0530

    feat: fetch user details for profile

--- keats/Controller/ProfileViewController.swift
+++ keats/Controller/ProfileViewController.swift
@@ -21,6 +21,7 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         makeNonEditable()
+        fetchUserDetails()
     }
     
     @IBAction func editButtonTapped(_ sender: Any) {
@@ -75,5 +76,81 @@ class ProfileViewController: UIViewController {
         phoneTextField.isUserInteractionEnabled = false
         phoneTextField.borderStyle = .none
         phoneTextField.backgroundColor = .clear
+    }
+    
+    private func loadImage(url: URL, completion: @escaping (UIImage?) -> ()) {
+            //utilityQueue.async {
+                guard let data = try? Data(contentsOf: url) else { return }
+                let image = UIImage(data: data)
+
+                DispatchQueue.main.async {
+                    completion(image)
+                }
+            
+        }
+    
+    func fetchUserDetails() {
+        let url = URL(string: "https://keats-testing.herokuapp.com/api/user")
+        guard let requestUrl = url else { return }
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "GET"
+        
+        guard let token = UserDefaults.standard.string(forKey: "JWToken") else {return}
+        
+        request.setValue( "Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            if let responseJSON = responseJSON as? [String: Any] {
+                print(responseJSON)
+                let status = responseJSON["status"]
+                if status as! String != "error" {
+                    let userdata = responseJSON["data"]
+                    if let userresponseJSON = userdata as? [String: Any] {
+                        if let bio = userresponseJSON["bio"] as? String, let email = userresponseJSON["email"] as? String, let id = userresponseJSON["id"] as? String, let phone = userresponseJSON["phone_number"] as? String, let profile_pic = userresponseJSON["profile_pic"] as? String, let username = userresponseJSON["username"] as? String{
+                            
+                            print(id)
+                            
+                            guard let url = URL(string: profile_pic) else {
+                                print("error1")
+                                return}
+                            print(url)
+                            DispatchQueue.global().async {
+                                guard let data = try? Data(contentsOf: url) else {
+                                    print("error2")
+                                    return }
+                                guard let image = UIImage(data: data) else {
+                                    print("error3")
+                                    return }
+                                DispatchQueue.main.async {
+                                    self.profileImage.image = image
+                                }
+                            }
+                            
+                            DispatchQueue.main.async {
+                                self.bioTextField.text = bio
+                                self.emailTextField.text = email
+                                self.nameTextField.text = username
+                                self.phoneTextField.text = phone
+                                
+                            }
+                            
+                            
+                        }
+                        
+                        
+                                                
+                        
+                    }
+                }
+                
+            }
+        }
+        task.resume()
     }
 }
