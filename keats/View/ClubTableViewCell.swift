9d966cef7769b82923538a90a8a627345259cc64
Swamita <swamita2001@gmail.com>
2021-04-29 22:56:54 +0530

    feat: club cell

---
+++ keats/View/ClubTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ClubTableViewCell.swift
+//  keats
+//
+//  Created by Swamita on 29/04/21.
+//
+
+import UIKit
+
+class ClubTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
